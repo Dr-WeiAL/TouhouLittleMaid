32a080acd5b3760dcba66008ba58438f59175bf1
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-04-19 15:40:02 +0800

    clean up compat

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/l2backpack/L2BackpackCompat.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/l2backpack/L2BackpackCompat.java
@@ -2,45 +2,34 @@ package com.github.tartaricacid.touhoulittlemaid.compat.l2backpack;
 
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.init.registry.CompatRegistry;
-import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import org.jetbrains.annotations.Nullable;
 
 public class L2BackpackCompat {
-    private static boolean IS_LOADED = false;
+	private static boolean IS_LOADED = false;
 
-    public static void init() {
-        IS_LOADED = true;
-        if (ModList.get().isLoaded(CompatRegistry.CURIOS)) {
-            L2BackpackCuriosCompat.init();
-        }
-    }
+	public static void init() {
+		IS_LOADED = true;
+		if (ModList.get().isLoaded(CompatRegistry.CURIOS)) {
+			MinecraftForge.EVENT_BUS.register(new L2BackpackHandlers());
+		}
+	}
 
-    public static boolean isLoaded() {
-        return IS_LOADED;
-    }
+	public static boolean isLoaded() {
+		return IS_LOADED;
+	}
 
-    public static boolean isBackpack(ItemStack stack) {
-        if (isLoaded()) {
-            return L2BackpackCompatInner.isBackpack(stack);
-        }
-        return false;
-    }
+	/**
+	 * 获取女仆的L2背包虚拟物品栏，用于直接读取背包内物品而不提取
+	 */
+	@Nullable
+	public static IItemHandlerModifiable getBackpackInventory(EntityMaid maid) {
+		if (!isLoaded()) {
+			return null;
+		}
+		return L2BackpackHandlers.getBackpackInventory(maid);
+	}
 
-    public static boolean isDimensionalStorage(ItemStack stack) {
-        if (isLoaded()) {
-            return L2BackpackCompatInner.isDimensionalStorage(stack);
-        }
-        return false;
-    }
-
-    /**
-     * 获取女仆的L2背包虚拟物品栏，用于直接读取背包内物品而不提取
-     */
-    public static IItemHandler getBackpackInventory(EntityMaid maid) {
-        if (!isLoaded()) {
-            return null;
-        }
-        return L2BackpackHandlers.getBackpackInventory(maid);
-    }
 }
